91aee446f0ee317201a9d408a09b41e8dfe5f23f
anish2mathew <anish2mathew@gmail.com>
2020-05-02 18:06:34 +0530

    branch name anish

--- src/test/java/org/greens/Sample.java
+++ src/test/java/org/greens/Sample.java
@@ -4,6 +4,6 @@ public class Sample {
 	public static void main(String[] args) {
 		System.out.println("git project");
 		System.out.println("successfully executed");
+		System.out.println("branch successfully changed from master to anish");
 	}
-
-}
+	}
