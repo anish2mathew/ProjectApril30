77e7f14f865493d54b7c29c4263c0c201ef67613
anish2mathew <anish2mathew@gmail.com>
2020-05-04 17:25:08 +0530

    code added in mathew

--- src/test/java/org/greens/Sample.java
+++ src/test/java/org/greens/Sample.java
@@ -5,5 +5,7 @@ public class Sample {
 		System.out.println("git project");
 		System.out.println("successfully executed");
 		System.out.println("branch successfully changed from master to anish");
+		System.out.println("branch changed to mathew");
+		System.out.println("successfully executed");
 	}
 	}
